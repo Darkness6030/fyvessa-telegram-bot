e782545ee46f420c751b44a7aa639fc92d32eeac
Darkness <inflexibled@gmail.com>
2026-08-09 15:59:55 +0300

    Add telegram api url

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,9 @@
+from typing import Optional
+
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
+from aiogram.client.telegram import TelegramAPIServer
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.utils.callback_answer import CallbackAnswerMiddleware
@@ -10,6 +14,7 @@ from rewire import config, simple_plugin
 @config
 class Config(BaseModel):
     token: str
+    api_url: Optional[str] = None
 
 
 plugin = simple_plugin()
@@ -17,8 +22,13 @@ plugin = simple_plugin()
 
 @plugin.setup()
 async def create_bot() -> Bot:
+    session = AiohttpSession(api=TelegramAPIServer.from_base(Config.api_url), limit=1024) \
+        if Config.api_url \
+        else AiohttpSession(limit=1024)
+
     return Bot(
         token=Config.token,
+        session=session,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
 
